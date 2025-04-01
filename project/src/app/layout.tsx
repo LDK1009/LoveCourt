4dc3f27155acd3fa2e837831bb26c84a58130c26
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-01 17:01:24 +0900

    ✨ Feat : 구글 애드센스 코드 추가

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4615072495258371"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
       </head>
       <body>
         <AppRouterCacheProvider>
