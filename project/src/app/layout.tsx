c3dbf58eaed82fe16d25dab900de1a580e666767
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-09 08:36:43 +0900

    ✨ Feat : 전환 태그 삽입

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -5,16 +5,20 @@ import CommonBottomNavigation from "@/components/common/CommonBottomNavigation";
 import ClientSnackbarProvider from "@/lib/ClientSnackbarProvider";
 import Header from "@/components/common/Header";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 // SEO 메타데이터
 export const metadata: Metadata = {
   manifest: "/manifest.json",
   title: "연애재판 | 연인과의 갈등, AI 판사가 공정하게 판결해드려요",
-  description: "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
-  keywords: "연애 재판, 커플 갈등 판결, 연애 분쟁, 연인 싸움, 객관적 판단, AI 판사, 연애 판결, 20대 연애, 데이트 갈등, 연애 법정",
+  description:
+    "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
+  keywords:
+    "연애 재판, 커플 갈등 판결, 연애 분쟁, 연인 싸움, 객관적 판단, AI 판사, 연애 판결, 20대 연애, 데이트 갈등, 연애 법정",
   openGraph: {
     title: "연애재판 | 연인과의 갈등, AI 판사가 공정하게 판결해드려요",
-    description: "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
+    description:
+      "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
     url: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
     images: [
       {
@@ -29,7 +33,8 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "연애재판 | 연인과의 갈등, AI 판사가 공정하게 판결해드려요",
-    description: "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
+    description:
+      "누구 잘못인지 헷갈리는 연애 갈등, 이제 AI 판사에게 맡겨보세요. 객관적인 판결로 당신의 연애 분쟁을 해결해드립니다.",
     images: [`${process.env.NEXT_PUBLIC_API_BASE_URL}/img/og.png`],
   },
   robots: {
@@ -51,8 +56,20 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4615072495258371"
-     crossOrigin="anonymous"></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4615072495258371"
+          crossOrigin="anonymous"
+        ></script>
+        <Script id="google-conversion-tracking" strategy="afterInteractive">
+          {`
+            gtag('event', 'conversion', {
+              'send_to': 'AW-16921971256/EDJPCJaHr6kaELiUg4U_',
+              'value': 1.0,
+              'currency': 'KRW'
+            });
+          `}
+        </Script>
       </head>
       <body>
         <AppRouterCacheProvider>
