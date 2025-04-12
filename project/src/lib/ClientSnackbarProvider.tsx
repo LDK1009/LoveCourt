90089d31059c38429fb06aa3662548c4bd36351b
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-12 09:49:01 +0900

    🎨 Style : 스낵바 위치 변경

--- project/src/lib/ClientSnackbarProvider.tsx
+++ project/src/lib/ClientSnackbarProvider.tsx
@@ -8,7 +8,7 @@ const ClientSnackbarProvider = () => {
       <SnackbarProvider
         maxSnack={3} // 최대 누적 스택
         autoHideDuration={1000} // 지속시간
-        // anchorOrigin={{ vertical: "bottom", horizontal: "left" }} // 스낵바 위치 지정
+        anchorOrigin={{ vertical: "top", horizontal: "center" }} // 스낵바 위치 지정
         // preventDuplicate={true} // 중복 메시지 표시 여부
         // hideIconVariant = {false} // 아이콘 숨김 여부
         // transitionDuration={{enter:300, exit:3000}} // 애니메이션 시간 변경 (ms)
